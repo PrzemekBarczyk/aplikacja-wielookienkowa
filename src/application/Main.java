b8ba04bc271477f3f7fa2dc5ddbe9cb841b20f44
przemekbarczyk <przemek.barczyk@gmail.com>
2020-08-22 13:31:46 +0200

    Utworzono pakiet application i przeniesiono do niego klase Main

---
+++ src/application/Main.java
@@ -0,0 +1,21 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+//import application.mainmenu.MainMenuController;
+//import application.mainmenu.MainMenuModel;
+//import application.mainmenu.MainMenuView;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) {
+
+        primaryStage.setTitle("Aplikacja wielookienkowa");
+//        primaryStage.setScene(new MainMenuController(new MainMenuView(), new MainMenuModel(), primaryStage).getScene());
+        primaryStage.show();
+    }
+}
